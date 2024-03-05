8e702a32b68f1bfd225fe44d87548e7b05da61db
Омиркул Рауан <ommirkulr@gmail.com>
2024-03-06 01:45:43 +0500

    Added models

--- homepages/models.py
+++ homepages/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+class product_category(models.Model):
+    """Category description"""
+    name = models.CharField(max_length=50, primary_key = True)
+    product = models.ManyToManyField('product', related_name='name')
+
+    """Category changes dates"""
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now_add = True)
+    deleted_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Product_categories'
+
+
+class product(models.Model):
+    """Products description"""
+    name = models.CharField(max_length = 100)
+    desc = models.TextField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='homepages/static/dist/img')
+    
+    """Product changes dates"""
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now_add = True)
+    deleted_at = models.DateTimeField(auto_now_add = True)
+
+    """Product Rels"""
+    category = models.ManyToManyField("product_category", related_name='name')
+    
+    def __str__(self):
+        return self.name[:50] + ' ...'
+
+
+class discount(models.Model):
+    """Dicount description"""
+    product_name = models.ForeignKey(product, on_delete=models.CASCADE)
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
+    active = models.BooleanField()
+
+    """Discount changes dates"""
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now_add = True)
+    deleted_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return str(self.discount_percent) + "%" + "  -  " + self.product_name.name
+    
+
+class product_inventory(models.Model):
+    """Inventory description"""
+    product_name = models.OneToOneField(product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    """Inventory changes dates"""
+    created_at = models.DateTimeField(auto_now_add = True)
+    modified_at = models.DateTimeField(auto_now_add = True)
+    deleted_at = models.DateTimeField(auto_now_add = True)
+    
+    class Meta:
+        verbose_name_plural = 'Product inventories'
+
+    def __str__(self):
+        return str(self.quantity) + " - " + self.product_name.name
--- shop/urls.py
+++ shop/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Luffy'
+admin.site.index_title = 'Will become the Priate King'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepages.urls', namespace='homepages')),
