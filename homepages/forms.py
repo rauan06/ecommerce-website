82776555db4cfdc946808302cad2c892bf19270e
Rauan <138851982+rauan06@users.noreply.github.com>
2024-03-07 21:36:49 +0500

    Corrected the discount math

--- homepages/forms.py
+++ homepages/forms.py
@@ -3,10 +3,8 @@ from django import forms
 SIZE_CHOICES = [('xs','XS'),('m','M'), ('xl','XL')]
 
 class Cart(forms.Form):
-    sizes = forms.ChoiceField(label="",  label_suffix=" ", widget=forms.RadioSelect(attrs={'class':'btn-check'}), choices=SIZE_CHOICES)
+    sizes = forms.ChoiceField(label="",  label_suffix=" ", widget=forms.RadioSelect(), choices=SIZE_CHOICES)
     quantity = forms.IntegerField(label=" ",  label_suffix=" ", widget=forms.NumberInput())
-
-
 
 
 # class EntryForm(forms.ModelForm):
