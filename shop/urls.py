6b62028f933793ed5a96b57ab5e2d4f32afe60c3
Rauan <138851982+rauan06@users.noreply.github.com>
2024-03-11 22:58:05 +0500

    Small changes

--- shop/urls.py
+++ shop/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from homepages import views
 
 admin.site.site_header = 'Luffy'
 admin.site.index_title = 'Will become the Priate King'
