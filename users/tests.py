128de98e3ebc31f4aac5279a0640d6fef4ac52a8
Rauan <138851982+rauan06@users.noreply.github.com>
2024-03-18 04:47:25 +0500

    Small changes

--- users/tests.py
+++ users/tests.py
@@ -4,18 +4,34 @@ from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestAuth(TestCase):
+    """Testing authentication"""
+
     def setUp(self):
+        self.url_register = reverse("users:register")
         self.url_login = reverse("users:login")
+        self.url_logout = reverse("users:logout_view")
 
-        User.objects.create(
-            username= 'testuser',
-            password= 'testpass',
-        )
-
-        self.client.login(username= 'testuser', password= 'testpass')
-
-    def test_login(self):
-        response = self.client.get(reverse("homepages:index"))
+    def test_register(self):
+        response = self.client.post(self.url_register, {'username':'testuser', 'password1':'testpass','password2':'testpass'},
+                                    follow=True)
 
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "logout")
+
+    def test_login(self):
+        # Testing if the successful login credentials redirect to the index page, and shows the 'logout' message
+        self.client.login(username= 'testuser', password= 'testpass')
+        response = self.client.post(self.url_login, {'username':'<USERNAME>', 'password':'<PASSWORD>'}, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
         self.assertContains(response, 'logout')
+
+    def test_logout(self):
+        self.client.post(self.url_login, {'username':'<USERNAME>', 'password':'<PASSWORD>'})
+        response = self.client.get(self.url_logout, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, 'Login')
+    
