34ba2a9134dd4735af01930b8171f0f2d4f193db
Rauan <138851982+rauan06@users.noreply.github.com>
2024-03-18 03:49:13 +0500

    Started tests for users

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,19 @@
+from django.urls import reverse
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestAuth(TestCase):
-    def setUP(self):
-        pass
+    def setUp(self):
+        self.login_url = reverse("users:login")
+
+        User.objects.create(
+            username= 'testuser',
+            password= 'testpass',
+        )
+
+    def test_login(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpass'})
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
