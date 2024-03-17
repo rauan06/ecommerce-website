6c32a200a24b6102689be1c4da9db009042687d9
Rauan <138851982+rauan06@users.noreply.github.com>
2024-03-18 04:14:16 +0500

    Small changes

--- users/tests.py
+++ users/tests.py
@@ -5,15 +5,17 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class TestAuth(TestCase):
     def setUp(self):
-        self.login_url = reverse("users:login")
+        self.url_login = reverse("users:login")
 
         User.objects.create(
             username= 'testuser',
             password= 'testpass',
         )
 
+        self.client.login(username= 'testuser', password= 'testpass')
+
     def test_login(self):
-        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpass'})
-        
+        response = self.client.get(reverse("homepages:index"))
+
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, 'logout')
