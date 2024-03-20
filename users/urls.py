f5f50c0e78b29efd80d538ca24b4da32885d6b1f
Rauan <138851982+rauan06@users.noreply.github.com>
2024-03-20 08:20:34 +0500

    Commented the code

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html', next_page="homepages:index", redirect_authenticated_user=True), name="login"),
     path('logout/', LogoutView.as_view(next_page="homepages:index"), name="logout_view"),
     path('register/', views.register, name="register"),
-    path('checkout/', views.checkout, name="checkout")
+    path('checkout/', views.checkout, name="checkout"),
+    path('orders/', views.orders, name="orders")
 ]
--- users/views.py
+++ users/views.py
@@ -9,6 +9,7 @@ from django.urls import reverse
 # Create your views here.
 @login_required
 def checkout(request):
+    """Function for checkout"""
     if 'cart_items' in request.session:
         total = 0
         
@@ -26,22 +27,39 @@ def checkout(request):
                 int(request.session['cart_items'][values]['price'])
         print(request.session['cart_items'])       
         return render(request, 'checkout.html', {'cart_items': request.session['cart_items'], 'total': total})
+    
+    # User entered the url directly
     raise Http404
 
 
 def register(request):
+    """Function for creating a user and authenticating"""
     if request.user.is_authenticated:
+        # Redirect user to the homepage if user is already registered
         return HttpResponseRedirect(reverse("homepages:index"))
+    
     if request.method == 'POST':
+        # User sent the form
         form = UserCreationForm(request.POST)
+        
+        # User filled the fields correctly 
         if form.is_valid():
+            # User is saved in the database
             form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
+            # Authericating the user, so the user doesn't have to login after reg.
             auth_user = authenticate(username=username, password=password)
             login(request, auth_user)
             return HttpResponseRedirect(reverse('homepages:index'))
+    
+    # User just opened the page, send the reg. form structure 
     else: form = UserCreationForm()
 
     context = {'form': form}
     return render(request, 'register.html', context)
+
+
+def orders(request):
+    """Functions to show the user's orders"""
+    return render(request, 'order.html')
