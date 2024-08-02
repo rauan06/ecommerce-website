6f17f65ee1b1804b3dd03bcd90128db6a45a4d56
rauan <ommirkulr@gmail.com>
2024-08-02 19:14:40 +0500

    Small changes

--- homepages/tests.py
+++ homepages/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-from django.contrib.auth.models import User
 from .models import product, product_category, discount
 from . import views
 
